6a2b3bd1a6c93cd99e91726af7158a695e609c8a
Yash Sabne <yashsabne2006@gamil.com>
2025-08-05 12:29:20 +0530

    dropzone err

--- client-side/src/components/TextEditor.jsx
+++ client-side/src/components/TextEditor.jsx
@@ -3,7 +3,7 @@ import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
 import Image from '@tiptap/extension-image';
 import Link from '@tiptap/extension-link';
-import Dropzone from './Dropzone';
+import Dropzone from './Dropzone.jsx';
 
 const MenuBar = ({ editor, onImageUpload }) => {
   if (!editor) return null;
