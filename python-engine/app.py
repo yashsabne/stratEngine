59b1355bb80a12cc064347e5709caf8c94082938
Yash Sabne <138506637+yashsabne@users.noreply.github.com>
2025-08-07 09:29:53 +0530

    Update app.py

--- python-engine/app.py
+++ python-engine/app.py
@@ -44,5 +44,5 @@ def pricing():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
